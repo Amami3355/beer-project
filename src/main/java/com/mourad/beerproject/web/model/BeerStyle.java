42724ca112a589fb50cbf6f12ca9565db0fbef7f
Amami3355 <amamimourad13@gmail.com>
2024-01-22 12:00:14 +0100

    Creating BeerDto, BeerPagedList classes Creating BeerStyle class

---
+++ src/main/java/com/mourad/beerproject/web/model/BeerStyle.java
@@ -0,0 +1,14 @@
+package com.mourad.beerproject.web.model;
+
+public enum BeerStyle {
+    LAGER,
+    PILSNER,
+    STOUT,
+    GOSE,
+    PORTER,
+    ALE,
+    WHEAT,
+    IPA,
+    PALE_ALE,
+    SAISON
+}
